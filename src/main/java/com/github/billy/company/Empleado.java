32a30b4f0aa9f790831bd795fd03aa53705d89bf
gguzmans <gguzmans@emeal.nttdata.com>
2022-11-18 23:42:04 -0500

    Se corrige el calculo de la retencion. Se validan las entradas

--- src/main/java/com/github/billy/company/Empleado.java
+++ src/main/java/com/github/billy/company/Empleado.java
@@ -1,5 +1,7 @@
 package com.github.billy.company;
 
+import java.util.Objects;
+
 public class Empleado {
 
   public static final int MARGEN_INFERIOR = 1_000_000;
@@ -13,9 +15,20 @@ public class Empleado {
   private double horasTrabajadas;
 
   public Empleado(String nombreCompleto, double salario, double horasTrabajadas) {
+    validarEntradas(nombreCompleto, salario, horasTrabajadas);
     this.nombreCompleto = nombreCompleto;
     this.salario = salario;
     this.horasTrabajadas = horasTrabajadas;
+  }
+
+  private void validarEntradas(String nombreCompleto, double salario, double horasTrabajadas) {
+    Objects.requireNonNull(nombreCompleto, "nombreCompleto no puede ser null");
+    if (salario <= 0) {
+      throw new RuntimeException("salario debe ser mayor a cero");
+    }
+    if (horasTrabajadas <= 0) {
+      throw new RuntimeException("horasTrabajadas debe ser mayor a cero");
+    }
   }
 
   @Override
@@ -24,34 +37,34 @@ public class Empleado {
 
     builder.append("DESPRENDIBLE DE PAGO\n");
     builder.append("_____________________\n");
-    builder.append("Nombre: ").append(this.nombreCompleto).append("\n");
-    builder.append("Salario: ").append(this.salario).append("\n");
-    builder.append("Horas trabajadas: ").append(this.horasTrabajadas).append("\n");
-    builder.append("Valor hora: ").append(this.calcularValorHora()).append("\n");
-    builder.append("Auxilio de transporte: ").append(this.calcularAuxilioTransporte()).append("\n");
-    builder.append("Retención: ").append(this.calcularRetencion()).append("\n");
-    builder.append("Total a pagar: ").append(this.calcularTotalSalario()).append("\n");
+    builder.append("Nombre: ").append(nombreCompleto).append("\n");
+    builder.append("Salario: ").append(salario).append("\n");
+    builder.append("Horas trabajadas: ").append(horasTrabajadas).append("\n");
+    builder.append("Valor hora: ").append(calcularValorHora()).append("\n");
+    builder.append("Auxilio de transporte: ").append(calcularAuxilioTransporte()).append("\n");
+    builder.append("Retención: ").append(calcularRetencion()).append("\n");
+    builder.append("Total a pagar: ").append(calcularTotalSalario()).append("\n");
 
     return builder.toString();
   }
 
   private double calcularValorHora() {
-    return this.salario / HORAS_LABORALES_POR_MES;
+    return salario / HORAS_LABORALES_POR_MES;
   }
 
   private double calcularAuxilioTransporte() {
-    return this.salario <= MARGEN_INFERIOR ? SUBSIDIO_TRANSPORTE : CERO;
+    return salario <= MARGEN_INFERIOR ? SUBSIDIO_TRANSPORTE : CERO;
   }
 
   private double calcularRetencion() {
-    return this.salario > MARGEN_SUPERIOR ? this.salario * PORCENTAJE_RETENCION : CERO;
+    return salario > MARGEN_SUPERIOR ? (calcularValorHora() * horasTrabajadas * PORCENTAJE_RETENCION) : CERO;
   }
 
   private double calcularTotalSalario() {
-    return this.calcularSalarioNeto() + this.calcularAuxilioTransporte() - this.calcularRetencion();
+    return calcularSalarioNeto() + calcularAuxilioTransporte() - calcularRetencion();
   }
 
   private double calcularSalarioNeto() {
-    return calcularValorHora() * this.horasTrabajadas;
+    return calcularValorHora() * horasTrabajadas;
   }
 }
